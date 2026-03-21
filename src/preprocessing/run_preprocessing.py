36def942566bb13d333507d2876f1320c871d035
alpha-coder38 <paymodeharshad00@gmail.com>
2026-03-21 13:47:33 +0530

    updated utils.py

--- src/preprocessing/run_preprocessing.py
+++ src/preprocessing/run_preprocessing.py
@@ -9,7 +9,7 @@ from argparse import ArgumentParser
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--data_path", type=str, required=True)    #Path to the raw/original data
-    parser.add_argument("--event_tables_path", type=str, required=True)  #Path to raw/original dataset
+    parser.add_argument("--event_tables_path", type=str, required=True)  #Path to save start and stope timestamps 
     parser.add_argument("--preprocessed_edf_path", type=str, required=True) #Path to save pre processed edf files of every recording for each patient
     parser.add_argument("--final_npy_path", type=str, required=True) #Saves every edf file as numpy file
     parser.add_argument("--annotation_files_path", type=str, required=True) #Path to record file which contatins names of seizure files for each patient.
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     preprocessed_edf_path = Path(args.preprocessed_edf_path)
     final_npy_path = Path(args.final_npy_path)
     annotation_files_path = Path(args.annotation_files_path)
-    get_annotation_files(event_tables_path, annotation_files_path)
+    get_annotation_files(data_path, event_tables_path)
     preprocess_dataset_all(
         annotation_files_path, data_path, preprocessed_edf_path
     )
