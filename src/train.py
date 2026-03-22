8004b92668cea42f2b3ac1e986610bc66e4e9ba0
alpha-coder38 <paymodeharshad00@gmail.com>
2026-03-22 13:03:53 +0530

    train wandb path updated

--- src/train.py
+++ src/train.py
@@ -26,7 +26,7 @@ warnings.filterwarnings(
     "ignore", ".*does not have many workers.*"
 )  # DISABLED ON PURPOSE
 torch_geometric.seed_everything(42)
-api_key_file = open("wandb_api_key.txt", "r")
+api_key_file = open("/kaggle/working/eeg_detection/src/wandb_api_key.txt", "r")
 API_KEY = api_key_file.read()
 
 api_key_file.close()
