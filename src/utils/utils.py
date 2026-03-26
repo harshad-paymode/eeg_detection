2eaf9898b068c635e91aa079716b5127ee28f7dc
alpha-coder38 <paymodeharshad00@gmail.com>
2026-03-26 14:52:24 +0530

    introduced dropout

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -120,7 +120,7 @@ def reorder_channels_chbmit(raw):
     raw.rename_channels(ch_map)
     raw.reorder_channels(ch_demanded_order)
     montage = mne.channels.read_custom_montage(
-        Path("data/chb_mit_ch_locs.loc")
+        Path("eeg_detection/data/chb_mit_ch_locs.loc")
     )
     raw.set_montage(montage)
     return None
