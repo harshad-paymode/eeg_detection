50f00f9a3d6921df192ee033c544a953479821d2
alpha-coder38 <paymodeharshad00@gmail.com>
2026-04-01 22:11:18 +0530

    changed the model

--- src/models.py
+++ src/models.py
@@ -102,10 +102,10 @@ class GATv2Lightning(pl.LightningModule):
             nn.Dropout(0.5*dropout_on),  
             act_fn,
             Linear(512, 256, weight_initializer="kaiming_uniform"),
-            nn.Dropout(0.4*dropout_on),
+            nn.Dropout(0.2*dropout_on),
             act_fn,
             Linear(256, 128, weight_initializer="kaiming_uniform"),
-            nn.Dropout(0.4*dropout_on),
+            nn.Dropout(0.2*dropout_on),
             act_fn,
             Linear(
                 128,
