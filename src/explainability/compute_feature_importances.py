2474c0c3b2aba890c75ccb667ad2d98db2dd90e7
alpha-coder38 <paymodeharshad00@gmail.com>
2026-04-14 15:18:44 +0530

    pushing 10 epochs

--- src/explainability/compute_feature_importances.py
+++ src/explainability/compute_feature_importances.py
@@ -118,7 +118,7 @@ def compute_feature_importances(args):
                 prefetch_factor=None,
             )
             
-            gnn_explainer = GNNExplainer(epochs=200, lr=0.01)
+            gnn_explainer = GNNExplainer(epochs=10, lr=0.5)
             config = ModelConfig(
                 "multiclass_classification", task_level="graph", return_type="raw"
             )
